c8253c0213aa661dd469697ee48b030e905743e8
pawel.chudziak <pawel.chudziak@mga.com.pl>
2022-03-22 02:41:15 +0100

    fix scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(
     routes,
     {
-      scrollPositionRestoration: 'top'
+      scrollPositionRestoration: 'disabled'
     })],
   exports: [RouterModule]
 })
